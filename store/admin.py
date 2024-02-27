0ce5bb942c4c804b6a9e29350dbfbf819838dbba
Riley Aaron <pikachuassasin906@gmail.com>
2024-02-27 16:59:30 -0600

    Admin panel test

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Product
+from django.core.management import call_command
 
 # Register your models here.
 from .models import *
@@ -8,3 +10,10 @@ admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
+class ProductAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        call_command('collect_static_images')
+
+admin.site.register(Product, ProductAdmin)
